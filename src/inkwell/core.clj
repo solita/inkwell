fe698b25ec9d6114b07ef931321e96edffdc282b
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-03-24 11:48:54 +0200

    Give current state as argument to draw fn

--- src/inkwell/core.clj
+++ src/inkwell/core.clj
@@ -9,21 +9,18 @@
 (t/ann ^:no-check quil.core/sketch [Any * -> quil.Applet])
 (t/ann ^:no-check quil.core/sketch-close [QuilSketch -> Any])
 
-(t/def-alias State (t/Map t/Keyword Any))
-
-(defmacro state> [x]
-  `(t/ann-form ~x State))
-
-(t/ann-record InkwellSketch [quil-sketch :- QuilSketch
-                             running? :- (t/Atom1 Boolean)
-                             state :- (t/Agent1 State)])
+(t/ann-record [State] InkwellSketch [quil-sketch :- QuilSketch
+                                     running? :- (t/Atom1 Boolean)
+                                     state :- (t/Atom1 State)])
 (defrecord InkwellSketch [quil-sketch running? state]
   java.lang.AutoCloseable
   (close [sketch]
     (q/sketch-close (:quil-sketch sketch))
     nil))
 
-(t/def-alias Settings (HMap :mandatory {:draw [-> Any]}))
+(t/def-alias Settings (TFn [[State :variance :invariant]]
+                        (HMap :mandatory {:draw [State -> Any]
+                                          :initial-state State})))
 
 (t/non-nil-return java.io.StringWriter/toString :all)
 
@@ -33,26 +30,29 @@
     (.printStackTrace t (PrintWriter. string-writer))
     (.toString string-writer)))
 
-(t/ann make-sketch! [Settings -> InkwellSketch])
+(t/ann make-sketch! (All [State]
+                      [(Settings State) -> (InkwellSketch State)]))
 (defn make-sketch! [settings]
   (let [running? (t/atom> Boolean true)
-        main-thread-out *out*]
+        main-thread-out *out*
+        state (atom (:initial-state settings))]
     (t/letfn> [draw :- [-> Any]
                (draw []
                  (when @running?
                    (try
                      (when-let [user-draw (:draw settings)]
-                       (user-draw))
+                       (user-draw @state))
                      (catch Throwable t
                        (binding [*out* main-thread-out]
                          (println (throwable->string t)))
                        (reset! running? false)))))]
       (map->InkwellSketch {:quil-sketch (q/sketch
                                           :draw draw)
-                           :state (agent (state> {}))
+                           :state state
                            :running? running?}))))
 
-(t/ann stop! (All [[x :< InkwellSketch]] [x -> x]))
+(t/ann stop! (All [State [x :< (InkwellSketch State)]]
+               [x -> x]))
 (defn stop! [sketch]
   (reset! (:running? sketch) false)
   sketch)
--- test/inkwell/core_test.clj
+++ test/inkwell/core_test.clj
@@ -8,13 +8,22 @@
     @(:running? s) => true))
 
 (facts "The draw function"
-  (let [draw-count (atom 0)]
-    (with-open [s (make-sketch! {:draw #(swap! draw-count inc)})]
+  (let [draw-count (atom 0)
+        draw-arg (atom nil)]
+    (with-open [s (make-sketch! {:draw (fn [state]
+                                         (swap! draw-count inc)
+                                         (reset! draw-arg state))
+                                 :initial-state {:foo 0}})]
 
       (fact "is called while the sketch is running"
         (let [draw-count-before-sleep @draw-count]
           (Thread/sleep 100)
           (> @draw-count draw-count-before-sleep) => true))
+
+      (fact "is given the current state value as an argument"
+        (swap! (:state s) update-in [:foo] inc)
+        (Thread/sleep 100)
+        @draw-arg => {:foo 1})
 
       (fact "is not called while the sketch is stopped"
         (stop! s)
@@ -24,7 +33,7 @@
 
 (facts "If the draw function throws an exception"
   (with-out-str
-    (with-open [s (make-sketch! {:draw #(throw (Exception.))})]
+    (with-open [s (make-sketch! {:draw (fn [_] (throw (Exception.)))})]
       (Thread/sleep 100)
 
       (fact "the sketch is stopped"
