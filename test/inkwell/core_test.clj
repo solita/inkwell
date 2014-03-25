094656c901d2dc4ad9672e9e69f94813caec0444
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-03-25 08:22:46 +0200

    Remove sleeps from tests

--- test/inkwell/core_test.clj
+++ test/inkwell/core_test.clj
@@ -3,6 +3,18 @@
             [inkwell.core :refer :all])
   (:import java.io.StringWriter))
 
+(defn becomes [expected]
+  (fn [f]
+    (let [timeout-ms 100
+          wait-ms 20
+          start-ms (System/currentTimeMillis)
+          end-ms (+ start-ms timeout-ms)]
+      (loop []
+        (or (= (f) expected)
+            (when (< (System/currentTimeMillis) end-ms)
+              (Thread/sleep wait-ms)
+              (recur)))))))
+
 (fact "A new sketch starts out running"
   (with-open [s (make-sketch! {})]
     @(:running? s) => true))
@@ -17,27 +29,23 @@
 
       (fact "is called while the sketch is running"
         (let [draw-count-before-sleep @draw-count]
-          (Thread/sleep 100)
-          (> @draw-count draw-count-before-sleep) => true))
+          #(> @draw-count draw-count-before-sleep) => (becomes true)))
 
       (fact "is given the current state value as an argument"
         (swap! (:state s) update-in [:foo] inc)
-        (Thread/sleep 100)
-        @draw-arg => {:foo 1})
+        #(deref draw-arg) => (becomes {:foo 1}))
 
       (fact "is not called while the sketch is stopped"
         (stop! s)
         (let [draw-count-before-sleep @draw-count]
-          (Thread/sleep 100)
-          (= @draw-count draw-count-before-sleep) => true)))))
+          #(= @draw-count draw-count-before-sleep) => (becomes true))))))
 
 (facts "If the draw function throws an exception"
   (with-out-str
     (with-open [s (make-sketch! {:draw (fn [_] (throw (Exception.)))})]
-      (Thread/sleep 100)
 
       (fact "the sketch is stopped"
-        @(:running? s) => false)
+        #(deref (:running? s)) => (becomes false))
 
       (fact "the exception stacktrace is printed"
         (str *out*) => #"java.lang.Exception"))))
