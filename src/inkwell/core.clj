97cfbfa83bcf1fbe53ff66f2a81d284be0eccb4b
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-03-25 12:54:07 +0200

    Add :tick and :mouse-moved events

--- src/inkwell/core.clj
+++ src/inkwell/core.clj
@@ -8,6 +8,8 @@
 
 (t/ann ^:no-check quil.core/sketch [Any * -> quil.Applet])
 (t/ann ^:no-check quil.core/sketch-close [QuilSketch -> Any])
+(t/ann ^:no-check quil.core/mouse-x [-> t/AnyInteger])
+(t/ann ^:no-check quil.core/mouse-y [-> t/AnyInteger])
 
 (t/ann-record [State] InkwellSketch [quil-sketch :- QuilSketch
                                      running? :- (t/Atom1 Boolean)
@@ -18,8 +20,17 @@
     (q/sketch-close (:quil-sketch sketch))
     nil))
 
+(t/def-alias Position (Vector* t/AnyInteger t/AnyInteger))
+
+(t/def-alias TickEvent (HMap :mandatory {:type ':tick}))
+(t/def-alias MouseMovedEvent (HMap :mandatory {:type ':mouse-moved
+                                               :position Position}))
+
+(t/def-alias Event (U MouseMovedEvent TickEvent))
+
 (t/def-alias Settings (TFn [[State :variance :invariant]]
                         (HMap :mandatory {:draw [State -> Any]
+                                          :handle-event [State Event -> State]
                                           :initial-state State}
                               :optional {:title String})))
 
@@ -42,14 +53,29 @@
                  (when @running?
                    (binding [*out* main-thread-out]
                      (try
+                       (when-let [handle-event (:handle-event settings)]
+                         (swap! state handle-event {:type :tick}))
                        (when-let [user-draw (:draw settings)]
                          (user-draw @state))
+                       (catch Throwable t
+                         (println (throwable->string t))
+                         (reset! running? false))))))
+               mouse-moved :- [-> Any]
+               (mouse-moved []
+                 (when @running?
+                   (binding [*out* main-thread-out]
+                     (try
+                       (when-let [handle-event (:handle-event settings)]
+                         (swap! state handle-event {:type :mouse-moved
+                                                    :position [(quil.core/mouse-x)
+                                                               (quil.core/mouse-y)]}))
                        (catch Throwable t
                          (println (throwable->string t))
                          (reset! running? false))))))]
       (map->InkwellSketch {:quil-sketch (q/sketch
                                           :title (:title settings)
                                           :draw draw
+                                          :mouse-moved mouse-moved
                                           :target :perm-frame)
                            :state state
                            :running? running?}))))
--- test/inkwell/core_test.clj
+++ test/inkwell/core_test.clj
@@ -1,19 +1,39 @@
 (ns inkwell.core-test
   (:require [midje.sweet :refer :all]
+            [midje.checking.core :refer [as-data-laden-falsehood
+                                         data-laden-falsehood?]]
             [inkwell.core :refer :all])
-  (:import java.io.StringWriter))
+  (:import java.io.StringWriter
+           java.awt.Robot
+           java.awt.event.InputEvent))
 
-(defn becomes [expected]
-  (fn [f]
-    (let [timeout-ms 100
-          wait-ms 20
-          start-ms (System/currentTimeMillis)
-          end-ms (+ start-ms timeout-ms)]
-      (loop []
-        (or (= (f) expected)
-            (when (< (System/currentTimeMillis) end-ms)
-              (Thread/sleep wait-ms)
-              (recur)))))))
+(defn becomes-like
+  ([predicate]
+    (becomes-like predicate {}))
+  ([predicate options]
+    (fn [f]
+      (let [timeout-ms (:timeout options 100)
+            wait-ms (:wait options 20)
+            start-ms (System/currentTimeMillis)
+            end-ms (+ start-ms timeout-ms)]
+        (loop []
+          (let [actual (f)]
+            (or (predicate actual)
+                (if (< (System/currentTimeMillis) end-ms)
+                  (do
+                    (Thread/sleep wait-ms)
+                    (recur))
+                  (as-data-laden-falsehood {:notes [(str "actual: " (pr-str actual))]})))))))))
+
+(defn becomes
+  ([expected]
+    (becomes expected {}))
+  ([expected options]
+    (becomes-like (fn [actual]
+                    (= actual (if (fn? expected)
+                                (expected)
+                                expected)))
+                  options)))
 
 (fact "A new sketch starts out running"
   (with-open [s (make-sketch! {})]
@@ -21,10 +41,10 @@
 
 (facts "The draw function"
   (let [draw-count (atom 0)
-        draw-arg (atom nil)]
-    (with-open [s (make-sketch! {:draw (fn [state]
+        draw-args (atom nil)]
+    (with-open [s (make-sketch! {:draw (fn [& args]
                                          (swap! draw-count inc)
-                                         (reset! draw-arg state))
+                                         (reset! draw-args args))
                                  :initial-state {:foo 0}})]
 
       (fact "is called while the sketch is running"
@@ -33,7 +53,7 @@
 
       (fact "is given the current state value as an argument"
         (swap! (:state s) update-in [:foo] inc)
-        #(deref draw-arg) => (becomes {:foo 1}))
+        #(deref draw-args) => (becomes [{:foo 1}]))
 
       (fact "is not called while the sketch is stopped"
         (stop! s)
@@ -49,3 +69,44 @@
 
       (fact "the exception stacktrace is printed"
         (str *out*) => #"java.lang.Exception"))))
+
+(defn window-coordinate= [a b]
+  "Window decorations affect the cursor positions reported by the event, so this
+method is used to check that actual coordinates are within the right ballpark of
+the expected coordinates."
+  (< (Math/abs (- b a)) 25))
+
+(facts "The event handler"
+  (let [handle-return-value (atom 0)
+        handle-args (atom ())]
+    (with-open [s (make-sketch! {:handle-event (fn [& args]
+                                                 (swap! handle-args conj args)
+                                                 (swap! handle-return-value inc))})]
+      (let [frame @(:target-obj (meta (:quil-sketch s)))
+            [frame-x frame-y] ((juxt #(.x %) #(.y %)) (.getLocationOnScreen frame))
+            robot (Robot.)
+            focus-frame (fn focus-frame []
+                          (.mouseMove robot (+ frame-x 100) (+ frame-y 100))
+                          (.mousePress robot InputEvent/BUTTON1_MASK)
+                          (.mouseRelease robot InputEvent/BUTTON1_MASK)
+                          ;; Give the frame some time to receive focus
+                          (Thread/sleep 100))]
+
+        (fact "gets a :tick event before each screen update"
+          #(second (first @handle-args)) => (becomes {:type :tick}))
+
+        (fact "gets a :mouse-moved event when the mouse moves"
+          (reset! handle-args ())
+          (focus-frame)
+          (.mouseMove robot (+ frame-x 212) (+ frame-y 254))
+          #(deref handle-args)
+          => (becomes-like
+               (fn [actual]
+                 (some (fn [[_ event]]
+                         (and (= (:type event) :mouse-moved)
+                              (window-coordinate= 212 (get-in event [:position 0]))
+                              (window-coordinate= 254 (get-in event [:position 1]))))
+                       actual))))
+
+        (fact "its return value becomes the new state value"
+          #(deref (:state s)) => (becomes #(deref handle-return-value)))))))
