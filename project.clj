5edf8bb631306317aada22f7c8355d63064e5fe7
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-03-26 11:04:15 +0200

    Add Midje to project.clj

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.typed "0.2.40"]
                  [quil "1.7.0"]]
+  :plugins [[lein-midje "3.1.1"]]
   :core.typed {:check [inkwell.core]}
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
-  :aliases {"check" ["do" ["typed" "check"] "test"]})
+  :aliases {"check" ["do" ["typed" "check"] "midje"]})
